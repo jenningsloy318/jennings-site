45f0a49a2e7469fdc447fe2098ce50d8ceaebeee
Jennings Liu <jenningsloy318@gmail.com>
2024-11-14 12:12:49 +0800

    fix: create the template

---
+++ README.md
@@ -0,0 +1,8 @@
+# This my personal blog create with [Astrofy](https://github.com/manuelernestog/astrofy)
+
+I use this site to record my technical staff and diary.
+
+## Structure
+
+- [Blogs](./src/content/blog)
+- [Diary](./src/content/diary)
---
+++ src/content/config.ts
@@ -0,0 +1,33 @@
+import { z, defineCollection } from "astro:content";
+const blogSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string().optional(),
+  badge: z.string().optional(),
+  tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+    message: 'tags must be unique',
+  }).optional(),
+});
+
+
+const diarySchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string().optional(),
+  badge: z.string().optional(),
+  tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+    message: 'tags must be unique',
+  }).optional(),
+});
+export type BlogSchema = z.infer<typeof blogSchema>;
+export type diarySchema = z.infer<typeof blogSchema>;
+const blogCollection = defineCollection({ schema: blogSchema });
+const diaryCollection = defineCollection({ schema: blogSchema });
+export const collections = {
+  'blog': blogCollection,
+  'diary': blogCollection,
+}
